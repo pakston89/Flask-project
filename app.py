b7e719b062690abe965d45f137b48835cd90127a
Pau <pauserratosa5@gmail.com>
2022-01-06 18:43:14 +0100

    First commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/home")
+def home():
+    elements = []
+    for i in range(100):
+        elements.append(lambda i : i * 2 / 1.5)
+    return "Welcome to home!"
+
