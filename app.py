3cefd23922aa9f11319553c765dd3d70053c21b6
pakston89 <pauserratosa5@gmail.com>
2022-01-08 16:31:30 +0100

    Add inference method

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import time
 
 app = Flask(__name__)
 
@@ -15,3 +16,11 @@ def sum(value):
 def multiply(value):
 	result = value * 5
 	return "The result is {}".format(str(result))
+	
+@app.route("/inference/<int:n_inferences>")
+def inference(n_inferences):
+	vehicle_type = ["Car","Bus","Motorbike","Truck","Van"]
+	for i in range(n_inferences):
+		time.sleep(1)
+		return "Vehicle {}|{}|120Kmh".format(str(i),vehicle_type[i])
+	
