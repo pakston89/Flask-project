42508c0bf78c03c27ca4b6c6dad5f91eb4a76c6b
Pau <pauserratosa5@gmail.com>
2022-01-08 22:52:08 +0100

    Formatting

--- app.py
+++ app.py
@@ -22,9 +22,11 @@ def inference(epochs):
 	vehicle_type = ["Car","Bus","Motorbike","Truck","Van"]
 	vehicles_count = 0
 	infer_stdout = ""
+
 	for i in range(epochs):
 		for j in vehicle_type:
 			vehicles_count = vehicles_count + 1
 			infer_stdout = infer_stdout + "Vehicle {}|{}|120Kmh \n".format(str(vehicles_count),j)
+			
 	return infer_stdout
 	
