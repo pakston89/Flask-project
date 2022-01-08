d3a6cc6d640f7f6ac69ec9f094ae0a710fcfa4bc
pakston89 <pauserratosa5@gmail.com>
2022-01-08 15:24:55 +0100

    Change variable

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def home():
     return "Welcome to home!"
 
-@app.route("/sum/value")
+@app.route("/sum/<int:value>")
 def sum(value):
     result = value + 5
     return "The result is {}".format(str(result))
