bacaf5bbe4dbe713d8481d358b18e68676de7a18
pakston89 <pauserratosa5@gmail.com>
2022-01-08 22:21:32 +0100

    Modify method inference

--- app.py
+++ app.py
@@ -17,11 +17,14 @@ def multiply(value):
 	result = value * 5
 	return "The result is {}".format(str(result))
 	
-@app.route("/inference/<int:n_inferences>")
-def inference(n_inferences):
+@app.route("/inference/<int:epochs>")
+def inference(epochs):
 	vehicle_type = ["Car","Bus","Motorbike","Truck","Van"]
+	vehicles_count = 0
 	infer_stdout = ""
-	for i in range(n_inferences):
-		infer_stdout = infer_stdout + "Vehicle {}|{}|120Kmh \n".format(str(i),vehicle_type[i])
+	for i in range(epochs):
+		for j in vehicle_type:
+			vehicles_count = vehicles_count + 1
+			infer_stdout = infer_stdout + "Vehicle {}|{}|120Kmh \n".format(str(vehicles_count),j)
 	return infer_stdout
 	
